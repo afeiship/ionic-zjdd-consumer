c7bcb54afc0100bb30dd9b5e50f57526f1a7aa3d
afei <88603982@qq.com>
2016-04-26 13:27:39 +0800

    add number spinner

---
+++ app/scripts/directives/numberSpinnerDirective.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('IonicZjddConsumer')
+    .directive('numberSpinner', flow);
+
+  /** @ngInject */
+  function flow() {
+    var directive = {
+      restrict: 'E',
+      replace:true,
+      scope: {
+        value: '='
+      },
+      templateUrl: 'templates/views/components/number-spinner.html',
+      link: function (scope, element, attrs) {
+        scope.value = 0;
+
+        scope.plus=function(){
+          scope.value++;
+        };
+
+        scope.minus=function(){
+          if(scope.value){
+            scope.value--;
+          }
+        };
+      }
+    };
+
+    return directive;
+  }
+
+})();
