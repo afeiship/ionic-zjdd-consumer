0f5ada15dce22cdfab1b6ae5e6697468da9b6cae
afei <88603982@qq.com>
2016-05-02 22:29:11 +0800

    optimize code

--- app/scripts/controllers/myController.js
+++ app/scripts/controllers/myController.js
@@ -8,14 +8,6 @@
  */
 angular.module('IonicZjddConsumer')
   .controller('MyCtrl', function ($scope,$state,$rootScope) {
-    var vm=this;
-    vm.toSettings=toSettings;
-
-    function toSettings(){
-      //$state.go('settings');
-    }
-
-
 
     $scope.$on('$ionicView.beforeEnter', function() {
       $rootScope.tabHidden=false;
